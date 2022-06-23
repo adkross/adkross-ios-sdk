774ff5978fa85b3e2b8dee0546e4dd0f708a00a9
Furkan <furkankaplan@outlook.com>
2022-06-24 00:25:09 +0300

    Add Alamofire dependency

--- Package.swift
+++ Package.swift
@@ -18,13 +18,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire", from: Version(5, 6, 1))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "adkross-ios-sdk",
-            dependencies: [],
+            dependencies: ["Alamofire"],
             path: "Sources"),
         .testTarget(
             name: "adkross-ios-sdk-tests",
