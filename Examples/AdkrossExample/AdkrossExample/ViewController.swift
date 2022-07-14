144605f87cd00802da3605d07ed506d462f7a044
Furkan <furkankaplan@outlook.com>
2022-07-14 13:14:06 +0300

    Add example project for providing demo project

---
+++ Examples/AdkrossExample/AdkrossExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AdkrossExample
+//
+//  Created by Furkan Kaplan on 9.07.2022.
+//
+
+import UIKit
+import adkross_ios_sdk
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        Adkross.startWith(apiKey: <#T##String#>, appKey: <#T##String#>)
+    }
+
+
+}
+
