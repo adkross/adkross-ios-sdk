55708744cc0f75565051e3ca9b0389162be9b158
Furkan Kaplan <furkankaplan@outlook.com>
2023-04-20 17:31:01 +0300

    Update network environment

--- Sources/Networking/Environment.swift
+++ Sources/Networking/Environment.swift
@@ -8,10 +8,11 @@
 import Foundation
 
 struct Environment {
-    static let host: String = "https://api.adkross.com"
+    static let host: String = "https://api.adkross.com/sdk"
     
     struct Service {
         static let main: String = "/main"
         static let campaing: String = "/campaign"
+        static let fireEvent: String = "/fire-event"
     }
 }
