0439627534a5db3e5615b7b15a63bc4e950927c0
Furkan Kaplan <furkankaplan@outlook.com>
2023-06-27 15:43:35 +0300

    Implement Adkross SDK for example project

--- Examples/AdkrossExample/AdkrossExample/ViewController.swift
+++ Examples/AdkrossExample/AdkrossExample/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import adkross_ios_sdk
 
 class ViewController: UIViewController {
+    private var adkross: AdkrossInterstitial!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        adkross = AdkrossInterstitial(delegate: self)
+        adkross.load()
     }
 
+    @IBAction func keepContinueButtonTapped(_ sender: Any) {
+        self.adkross.present(fromRootViewController: self)
+    }
+}
 
+extension ViewController: AdkrossInterstitialDelegate {
+    func adkrossInterstitialAdFiredFailed(
+        with message: String,
+        evenType: EventType
+    ) {
+        print("adkrossInterstitialAdFiredFailed of \(evenType.rawValue) event with message: \(message)")
+    }
+
+    func adkrossInterstitialAdFired(with event: EventType) {
+        print("\(event.rawValue) fired")
+    }
+
+    func adkrossInterstitial(failedWith message: String) {
+        print("adkrossInterstitial(failedWith message: \(message)")
+    }
+
+    func adkrossInterstitialLoadedSuccessfully() {
+        print("adkrossInterstitialLoadedSuccessfully")
+    }
+
+    func adkrossInterstitialAdPresented() {
+        print("adkrossInterstitialAdPresented")
+    }
+
+    func adkrossInterstitialAdDismissed() {
+        print("adkrossInterstitialAdDismissed")
+    }
+
+    func adkrossInterstitialAdTapped() {
+        print("adkrossInterstitialAdDismissed")
+    }
 }
 
