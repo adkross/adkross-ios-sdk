819ca664b6479649f32c4d6825ea35851c677431
Furkan Kaplan <furkankaplan@outlook.com>
2023-06-27 15:42:40 +0300

    Add Kingfisher for image processing

--- Package.swift
+++ Package.swift
@@ -18,13 +18,14 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire", from: Version(5, 6, 1)),
+        .package(name: "Kingfisher", url: "https://github.com/onevcat/Kingfisher", from: Version(7, 0, 0)),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "adkross-ios-sdk",
-            dependencies: ["Alamofire"],
+            dependencies: ["Alamofire", "Kingfisher"],
             path: "Sources"),
         .testTarget(
             name: "adkross-ios-sdk-tests",
