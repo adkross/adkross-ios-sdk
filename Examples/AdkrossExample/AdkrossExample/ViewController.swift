53fae94e9d8289de611f298ac59437948e9fe814
Furkan Kaplan <furkankaplan@outlook.com>
2023-06-28 11:54:27 +0300

    Improve example project

--- Examples/AdkrossExample/AdkrossExample/ViewController.swift
+++ Examples/AdkrossExample/AdkrossExample/ViewController.swift
@@ -9,12 +9,36 @@ import UIKit
 import Adkross
 
 class ViewController: UIViewController {
+    @IBOutlet weak private var charachterLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak private var transparentView: UIView!
+    @IBOutlet weak private var missionFailedLabel: UILabel!
+    @IBOutlet weak private var keepContinueButton: UIButton!
+    @IBOutlet weak private var startAgainButton: UIButton!
+
     private var adkross: AdkrossInterstitial!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         adkross = AdkrossInterstitial(delegate: self)
         adkross.load()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        charachterLeadingConstraint.constant = 120
+
+        UIView.animate(withDuration: 2) {
+            self.view.layoutIfNeeded()
+        } completion: { isCompleted in
+            if isCompleted {
+                self.transparentView.isHidden = false
+                self.transparentView.isHidden = false
+                self.missionFailedLabel.isHidden = false
+                self.keepContinueButton.isHidden = false
+                self.startAgainButton.isHidden = false
+            }
+        }
     }
 
     @IBAction func keepContinueButtonTapped(_ sender: Any) {
